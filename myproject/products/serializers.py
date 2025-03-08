6b1bca4cb8fe99f1f1035f9b201f125b033593f6
sangam2ishra <sssangamkr.mishra@gmail.com>
2025-03-09 00:59:24 +0530

    added validation for price

--- myproject/products/serializers.py
+++ myproject/products/serializers.py
@@ -6,4 +6,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model=Product
         fields=['id', 'name', 'description', 'category', 'price', 'brand', 'quantity']
-        
+
+    def validate_price(self, value):
+        if value>1000:
+            raise serializers.ValidationError("price should be less than 1000")
+        return value
