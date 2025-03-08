193e55f359137e024af04bccf7c45d50acf1e1fa
sangam2ishra <sssangamkr.mishra@gmail.com>
2025-03-09 01:09:41 +0530

    added pagination using rest framework

--- myproject/products/views.py
+++ myproject/products/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from .pagination import StandardResultsSetPagination
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset=Product.objects.all().order_by('name')
     serializer_class=ProductSerializer
+    pagination_class=StandardResultsSetPagination
